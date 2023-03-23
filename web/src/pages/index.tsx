680bbe4b07a0f9e709df67c17f34a201459c2b7f
Gledson Lucas <gledsonlucas111@gmail.com>
2023-03-22 23:52:59 -0300

    adicionado estilos e logo do site

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,6 +1,3 @@
 export default function Home() {
-  return (
-    <>
-    </>
-  )
+  return <h1>Hello, World!</h1>;
 }
--- web/src/pages/product/[id].tsx
+++ web/src/pages/product/[id].tsx
@@ -1,9 +1,11 @@
 import { useRouter } from "next/router"
+import * as react from "react";
 
 export default function Product () {
     const { query } = useRouter();   
-
+   
     return(
-        <h1>Product: {JSON.stringify(query)}</h1>
+        <h1>Product: {JSON.stringify(query)}
+        </h1>
     )
 }
